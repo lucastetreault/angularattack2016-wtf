7168db89730134078b127920b40bb7a93fede43e
lucastetreault <lucastetreault@gmail.com>
2016-05-14 11:09:15 -0600

    Getting even more prettier!

--- src/app/code-compare/code-compare.component.ts
+++ src/app/code-compare/code-compare.component.ts
@@ -30,6 +30,8 @@ export class CodeCompareComponent implements OnInit, OnActivate{
       let lines = res.diff.split(/\r?\n/);
 
       let diff;
+      let leftLines;
+      let rightLines;
 
       lines.forEach(line => {
         if (line.startsWith('diff')) {
@@ -49,28 +51,37 @@ export class CodeCompareComponent implements OnInit, OnActivate{
           // Uh, what does this one mean? 
         } else if (line.startsWith('---') || line.startsWith('+++')) {
           // Do nothing I guess? 
-        } else if (line.startsWith('  ')) {
-
+        } else if (line.startsWith('@@')) {
+          let vars = line.split(' ');
+          let left = vars[1].split(',');
+          let right = vars[2].split(',');
+          leftLines = left[0].substr(1);
+          rightLines = right[0].substr(1);
+          diff.lines.left.push({summary: true, text: line});
+          diff.lines.right.push({summary: true, text: line});
+        } else if (line.startsWith(' ')) {
           diff = this.balanceDiff(diff);
-          diff.lines.left.push({ number: '', text: line });
-          diff.lines.right.push({ number: '', text: line });
-        } else if (line.startsWith('+ ')) {
-          diff.lines.right.push({number: '',
+          diff.lines.left.push({ number: leftLines, text: line });
+          diff.lines.right.push({ number: rightLines, text: line });
+          leftLines++;
+          rightLines++;
+        } else if (line.startsWith('+')) {
+          diff.lines.right.push({number: rightLines,
                     text: line,
                     color: '#eaffea',
                     lineNumberColor: '#dbffdb' });
-        } else if (line.startsWith('- ')) {
-          diff.lines.left.push({ number: '',
+          rightLines++;
+        } else if (line.startsWith('-')) {
+          diff.lines.left.push({ number: leftLines,
                     text: line,
                     color: '#ffecec',
                     lineNumberColor: '#ffdddd' });
+          leftLines++;
         }
       });
 
-      if (diff) {
-        diff = this.balanceDiff(diff);
-        that.diffs.push(diff);
-      }
+      diff = this.balanceDiff(diff);
+      that.diffs.push(diff);
     }
     );
   }
