cc878aba6cb3266bd94c8b6d11ed588fd4e13d2e
lucastetreault <lucastetreault@gmail.com>
2016-05-14 23:23:50 -0600

    Redirect to Github login is working - now what?

---
+++ README.txt
@@ -0,0 +1,6 @@
+Github OAuth details: 
+
+Client ID
+9b4410e16f2ebd31a513
+Client Secret
+49f3f5adb5df06b46e27e65ecd6076b72df61aea
--- src/app/pull-request-list/pull-request-list.component.ts
+++ src/app/pull-request-list/pull-request-list.component.ts
@@ -14,10 +14,14 @@ export class PullRequestListComponent implements OnInit {
 
   private pulls;
 
-  constructor(private pullService: PullsService) {}
+  constructor(private pullService: PullsService) { }
 
   ngOnInit() {
     this.pulls = this.pullService.getPullRequests();
+  }
+
+  login() {
+    window.location.href = <any>'https://github.com/login/oauth/authorize?client_id=9b4410e16f2ebd31a513&scope=public_repo,repo';
   }
 
 }
